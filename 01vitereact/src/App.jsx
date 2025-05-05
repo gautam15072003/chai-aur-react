98c5497a1adb699759bf1d47c300e2135cb91b39
gautam15072003 <gautamsaini1507@gmail.com>
2025-05-05 19:43:25 +0000

    here we learned about custom react ki kaise humne react banaya aur humne sikha ismein html injection in javascript ki kaise read krti hai

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,16 +1,16 @@
 import Chai from "./Chai"
 
 function App() {
- 
+ const userName = "kashish aur motivation"
 
   return (
     <>    {/* ye jo khaali tags hai ye fragements kehlaye gaye kyuki ye ek hi element export krr sakte hai  */}
 
     <Chai/>
-    <h3>chai aur code </h3>
+    <h3>chai aur code {userName}</h3> 
     <p> test para</p>
     </>
-    
+    // evaluated expression {userName}
   
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,38 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+
+function MyApp() {
+  return(
+    <div>
+      <h1> custom App ! </h1>
+    </div>
+  )
+}
+// const reactElement = {
+//   type: 'a',
+//   props:{
+//       href:'https://google.com',
+//       target:'_blank'
+//   },
+//   Children:'click me too visit google '
+// }
+const ekaurElement = "chai aur code"
+
+
+const reactElement = React.createElement(
+  'a',
+  {href:'https://google.com',target:'_blank'},
+  'click me too visit google',
+  'ekaurElement'
+)
+
+const anotherElement = (
+  <a href="https://google.com" target ='_blank'>visit google</a>
+) 
 
 createRoot(document.getElementById('root')).
 render(
   
-    <App />
-  )
+reactElement
+)
