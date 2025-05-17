d711ceb349faa45147422eccb2c0fe5ee26e05e9
gautam15072003 <gautamsaini1507@gmail.com>
2025-05-17 20:35:05 +0000

    hooks and component

---
+++ 06currencyconvertor/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import InputBox from './components/Index'
+import usecurrencyInfo from './hooks/useCurrencyInfo'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className= 'text-3xl bg-orange-500'>
+      test
+    </div>
+    </>
+  )
+}
+
+export default App
