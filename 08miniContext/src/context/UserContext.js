23c6ff50fc2ca7d2ce75e6881901d76e197e944f
Gautam <gautamsaini1507@gmail.com>
2025-05-22 18:09:18 +0000

    useContext hook and context api

---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const UserContext = React.createContext()
+
+export default UserContext;
+
+{/* <>
+<UserContext>
+  <Login/>
+  <Card>
+    <Data/>    provider bhi bnana padega iske liye aur provider ka kaam sirf data provide krna hota hai 
+  </Card>
+</UserContext>
+    
+</> */}
